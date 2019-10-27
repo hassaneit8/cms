f76199779dc170f01a2fcae28d7e26db80d68590
cmd <hassaneit8@gmail.com>
2019-10-27 06:49:49 +0200

    paginate blog view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,6 +10,6 @@ use Illuminate\Http\Request;
 class WelcomeController extends Controller
 {
     public function index(){
-        return view('welcome')->with('tags',Tag::all())->with('categories',Category::all())->with('posts',Post::all());
+        return view('welcome')->with('tags',Tag::all())->with('categories',Category::all())->with('posts',Post::simplePaginate(2));
     }
 }
